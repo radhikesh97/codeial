64a97d0ae99887a790e1bacb2d6681eaedd2b5c0
radhikesh97 <radhikesh1997@gmail.com>
2019-06-15 13:06:27 +0530

    added sign-up

--- contollers/users_controller.js
+++ contollers/users_controller.js
@@ -1,18 +1,52 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
     return res.render('user',{
         title: 'Profile'
     });
     //return res.end('<h1>Profile!!</h1>');
-};
+}
 
 module.exports.sign_up = function(req,res){
     return res.render('sign_up',{
         title: 'Codeial | Sign Up'
     })
-};
+}
 
 module.exports.sign_in = function(req,res){
     return res.render('sign_in',{
         title: 'Codeial | Sign In'
     })
-};
+}
+
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({
+        email: req.body.email
+    },function(err,user){
+        if(err){
+            console.log("Error in signing up");
+            return;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log("Error in signing up");
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            res.redirect('back');
+        }
+    })
+
+}
+
+module.exports.createSession = function(req,res){
+
+}
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,6 @@ const userConroller = require('../contollers/users_controller');
 router.get('/profile',userConroller.profile);
 router.get('/sign-up',userConroller.sign_up);
 router.get('/sign-in',userConroller.sign_in);
-
+router.post('/create',userConroller.create);
 
 module.exports = router;
